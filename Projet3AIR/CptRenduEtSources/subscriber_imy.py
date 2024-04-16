382008b20e682101aae8b5375785a8f7144d8ccf
Gabriel <gabrielgarrone670@gmail.com>
2024-04-16 14:12:11 +0200

    Ajout des procédures d'installation

---
+++ Projet3AIR/CptRenduEtSources/subscriber_imy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import rospy
+from sensor_msgs.msg import Imu  # Import du type de message pour Imu 
+
+def callback(data):             # Affiche en direct au travers de terminaux ROS les information dont nous avons besoin
+    rospy.loginfo("Received Imu data:")
+    rospy.loginfo("Linear Acceleration: x=%f, y=%f, z=%f", data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z)
+    rospy.loginfo("Angular Velocity: x=%f, y=%f, z=%f", data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z)
+    rospy.loginfo("Orientation: x=%f, y=%f, z=%f, w=%f", data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w)
+
+def listener():     # Definition de notre subscriber
+    rospy.init_node('subscriber_imu', anonymous=True)       # Nom de notre noeud
+    rospy.Subscriber("/carla/ego_vehicle/imu", Imu, callback)   # Nous indiquons à quelle publishers nous nous souscrivons 
+    rospy.spin()        # Permet au programme d'être en route continuellement
+
+if __name__ == '__main__':
+    listener()
---
+++ Projet3AIR/CptRenduEtSources/subscriber_speedometer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import rospy
+from std_msgs.msg import Float32  # Import du type de message pour Float32
+
+def callback(data):             # Affiche en direct au travers de terminaux ROS les information dont nous avons besoin
+    rospy.loginfo("Received data: %f", data.data)
+
+def listener():     # Definition de notre subscriber
+    rospy.init_node('subscriber_speedometer', anonymous=True)       # Nom de notre noeud
+    rospy.Subscriber("/carla/ego_vehicle/speedometer", Float32, callback)    # Nous indiquons à quelle publishers nous nous souscrivons 
+    rospy.spin()        # Permet au programme d'être en route continuellement
+
+if __name__ == '__main__':
+    listener()
